c9712afbedb3e69af416d1bd83b72b6d20ee5889
Daniel Danang <84694349+jaikolek@users.noreply.github.com>
2025-07-21 11:48:54 +0700

    [add] readonly attribute

---
+++ Assets/JaikolekUtils/Scripts/Attributes/ReadOnlyAttribute.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace JaikolekUtils.Attributes
+{
+    /// <summary>
+    /// Read Only attribute.
+    /// Attribute is use only to mark ReadOnly properties.
+    /// </summary>
+    public class ReadOnlyAttribute : PropertyAttribute { }
+}
