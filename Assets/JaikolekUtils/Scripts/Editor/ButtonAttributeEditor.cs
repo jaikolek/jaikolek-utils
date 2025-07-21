f0b34c906abc63d66f4004d160171167acbae1ee
Daniel Danang <84694349+jaikolek@users.noreply.github.com>
2025-07-21 18:07:35 +0700

    [update] button attribute logic

--- Assets/JaikolekUtils/Scripts/Editor/ButtonAttributeEditor.cs
+++ Assets/JaikolekUtils/Scripts/Editor/ButtonAttributeEditor.cs
@@ -25,7 +25,7 @@ namespace JaikolekUtils.Attributes
                 if (buttonAttr == null) continue;
 
                 var parameters = method.GetParameters();
-                string methodKey = method.Name;
+                string methodKey = GetMethodKey(targetType, method);
 
                 GUILayout.Space(buttonAttr.SpaceBefore);
 
@@ -33,41 +33,19 @@ namespace JaikolekUtils.Attributes
                     ? ObjectNames.NicifyVariableName(method.Name)
                     : buttonAttr.Label;
 
-                object[] args = new object[parameters.Length];
-                if (!methodParamsCache.TryGetValue(methodKey, out args))
+                if (!methodParamsCache.TryGetValue(methodKey, out var args) || args.Length != parameters.Length)
                 {
                     args = new object[parameters.Length];
                     for (int i = 0; i < args.Length; i++)
-                    {
                         args[i] = GetDefault(parameters[i].ParameterType);
-                    }
                     methodParamsCache[methodKey] = args;
                 }
 
                 for (int i = 0; i < parameters.Length; i++)
                 {
                     var param = parameters[i];
-                    Type type = param.ParameterType;
-
-                    GUILayout.BeginHorizontal();
-                    GUILayout.Label($"{param.Name} ({type.Name})", GUILayout.Width(150));
-
-                    if (type == typeof(int))
-                        args[i] = EditorGUILayout.IntField((int)args[i]);
-                    else if (type == typeof(float))
-                        args[i] = EditorGUILayout.FloatField((float)args[i]);
-                    else if (type == typeof(string))
-                        args[i] = EditorGUILayout.TextField((string)args[i]);
-                    else if (type == typeof(bool))
-                        args[i] = EditorGUILayout.Toggle((bool)args[i]);
-                    else if (type.IsEnum)
-                        args[i] = EditorGUILayout.EnumPopup((Enum)args[i]);
-                    else if (typeof(UnityEngine.Object).IsAssignableFrom(type))
-                        args[i] = EditorGUILayout.ObjectField((UnityEngine.Object)args[i], type, true);
-                    else
-                        EditorGUILayout.LabelField($"Unsupported param type: {type.Name}");
-
-                    GUILayout.EndHorizontal();
+                    string paramKey = $"{target.GetInstanceID()}__{methodKey}__{param.Name}";
+                    args[i] = DrawParameterField(paramKey, param.ParameterType, args[i], param.Name);
                 }
 
                 if (GUILayout.Button(label))
@@ -84,6 +62,77 @@ namespace JaikolekUtils.Attributes
             }
         }
 
+        private string GetMethodKey(Type type, MethodInfo method)
+        {
+            string paramTypes = string.Join("_", Array.ConvertAll(method.GetParameters(), p => p.ParameterType.Name));
+            return $"{type.FullName}.{method.Name}__{paramTypes}";
+        }
+
+        private object DrawParameterField(string key, Type type, object currentValue, string labelOverride = null)
+        {
+            string label = ObjectNames.NicifyVariableName(labelOverride ?? key);
+
+            switch (type)
+            {
+                case Type t when t == typeof(int):
+                    int intVal = SessionState.GetInt(key, currentValue is int i ? i : 0);
+                    intVal = EditorGUILayout.IntField(label, intVal);
+                    SessionState.SetInt(key, intVal);
+                    return intVal;
+
+                case Type t when t == typeof(float):
+                    float floatVal = SessionState.GetFloat(key, currentValue is float f ? f : 0f);
+                    floatVal = EditorGUILayout.FloatField(label, floatVal);
+                    SessionState.SetFloat(key, floatVal);
+                    return floatVal;
+
+                case Type t when t == typeof(string):
+                    string strVal = SessionState.GetString(key, currentValue as string ?? "");
+                    strVal = EditorGUILayout.TextField(label, strVal);
+                    SessionState.SetString(key, strVal);
+                    return strVal;
+
+                case Type t when t == typeof(bool):
+                    bool boolVal = SessionState.GetBool(key, currentValue is bool b && b);
+                    boolVal = EditorGUILayout.Toggle(label, boolVal);
+                    SessionState.SetBool(key, boolVal);
+                    return boolVal;
+
+                case Type t when t.IsEnum:
+                    string enumString = SessionState.GetString(key, (currentValue ?? Enum.GetValues(type).GetValue(0)).ToString());
+                    Enum enumVal = (Enum)Enum.Parse(type, enumString);
+                    enumVal = EditorGUILayout.EnumPopup(label, enumVal);
+                    SessionState.SetString(key, enumVal.ToString());
+                    return enumVal;
+
+                case Type t when typeof(UnityEngine.Object).IsAssignableFrom(t):
+                    string objKey = key + "_objID";
+                    int objID = SessionState.GetInt(objKey, (currentValue as UnityEngine.Object)?.GetInstanceID() ?? 0);
+                    UnityEngine.Object obj = EditorUtility.InstanceIDToObject(objID);
+                    obj = EditorGUILayout.ObjectField(label, obj, type, true);
+                    SessionState.SetInt(objKey, obj ? obj.GetInstanceID() : 0);
+                    return obj;
+
+                case Type t when t.IsClass && t.IsSerializable:
+                    object objInstance = currentValue ?? Activator.CreateInstance(t);
+                    EditorGUILayout.BeginVertical("box");
+                    EditorGUILayout.LabelField(ObjectNames.NicifyVariableName(t.Name), EditorStyles.boldLabel);
+                    foreach (var field in t.GetFields(BindingFlags.Public | BindingFlags.Instance))
+                    {
+                        string fieldKey = key + "." + field.Name;
+                        object fieldValue = field.GetValue(objInstance);
+                        object updated = DrawParameterField(fieldKey, field.FieldType, fieldValue, field.Name);
+                        field.SetValue(objInstance, updated);
+                    }
+                    EditorGUILayout.EndVertical();
+                    return objInstance;
+
+                default:
+                    EditorGUILayout.LabelField($"Unsupported param type: {type.Name}");
+                    return null;
+            }
+        }
+
         private object GetDefault(Type type)
         {
             if (type == typeof(string)) return "";
@@ -92,7 +141,8 @@ namespace JaikolekUtils.Attributes
             if (type == typeof(bool)) return false;
             if (type.IsEnum) return Enum.GetValues(type).GetValue(0);
             if (typeof(UnityEngine.Object).IsAssignableFrom(type)) return null;
-            return null; // default for unsupported types
+            if (type.IsClass && type.GetConstructor(Type.EmptyTypes) != null) return Activator.CreateInstance(type);
+            return null;
         }
     }
 }
